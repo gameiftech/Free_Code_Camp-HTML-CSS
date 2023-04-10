f9ff1e41089351fe7c4569a8186290bdb52839f9
Deivid Castro <126339810+gameiftech@users.noreply.github.com>
2023-04-10 16:31:50 -0300

    Exercicios HTML e CSS

---
+++ Exercicio Javascript/app.js
@@ -0,0 +1,9 @@
+<<<<<<< Updated upstream
+const myArray = [50, 60, 70];
+console.log(myArray[1])
+const myData = myArray[0];
+=======
+const myArray = [50, 60, 70];
+console.log(myArray[1])
+const myData = myArray[0];
+>>>>>>> Stashed changes
